c3b35e8adfdffd3a9e43c269439f4fb4694de6d2
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-02-07 01:01:58 +0200

    users no repeat in signup

--- src/components/control/Api.js
+++ src/components/control/Api.js
@@ -1,31 +1,32 @@
 
 const Api = {
    signup: (user) => {
-      wait(3000).then(()=> {
+      wait(3000).then(() => {
          let usersArr = JSON.parse(localStorage.getItem("Users")); //[]
          console.log(usersArr);
 
          if (usersArr) {
-            usersArr.forEach(element => {
-               if (user.name === element.name &&
-                  user.email === element.email &&
-                  user.password === element.password && 
-                  user.confirm_password === element.confirm_password) {
-                     return "this user is already existed";
-               } else {
-                  usersArr.push(user);
-                  let users = JSON.stringify(usersArr);
-                  localStorage.setItem("Users",users);
-                  return usersArr;
-               }
-            });
-            
+            let result = usersArr.find(element =>
+               user.name === element.name &&
+               user.email === element.email &&
+               user.password === element.password &&
+               user.confirm_password === element.confirm_password
+            );
 
+            if (!result) {
+               usersArr.push(user);
+               let users = JSON.stringify(usersArr);
+               localStorage.setItem("Users", users);
+               return usersArr;
+            } else if (result) {
+               return console.log("this user is already existed");
+            }
+            
          } else {
-            usersArr = [user];
-            let users = JSON.stringify(usersArr);
-            localStorage.setItem("Users",users);
-            return usersArr;
+               usersArr = [user];
+               let users = JSON.stringify(usersArr);
+               localStorage.setItem("Users", users);
+               return usersArr;
          }
       });
    },
@@ -36,11 +37,11 @@ const Api = {
 }
 
 function wait(duration) {
-   return new Promise((resolve,reject) => {
+   return new Promise((resolve, reject) => {
       if (duration < 0) {
          reject(new Error("wrong value"));
       }
-     setTimeout(resolve,duration);
+      setTimeout(resolve, duration);
    });
 }
 
