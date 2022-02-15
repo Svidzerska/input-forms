faafd25a45fe7d418649624aa4684039fd74a5b5
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2022-02-15 16:37:54 +0200

    todo list was done but without local storadge

--- src/components/js/ElementForm/Button.js
+++ src/components/js/ElementForm/Button.js
@@ -1,7 +1,7 @@
 function Button(props) {
 
    return (
-      <button onClick={props.onClick} item_number={props.item_number}>{props.text}</button>
+      <button onClick={props.onClick} index_item={props.index_item}>{props.text}</button>
    )
 }
 
--- src/components/js/ToDoList/ToDoList.js
+++ src/components/js/ToDoList/ToDoList.js
@@ -3,66 +3,120 @@ import InputHooks from "../ElementForm/Input_component_hooks";
 import "../../css/ToDoListStyles/todoPage.css";
 import Button from "../ElementForm/Button";
 
-const valuesArray = [];
+let valuesArray = [];
+
+
 
 function ToDoList(props) {
-   console.log(props);
+   // console.log(props);
+
+   // const getObj = localStorage.getItem(props.authedName.name);
+   // const dataFromStorage = JSON.parse(getObj);
+   
+
+
+   const initValuesArray = [];
 
    const [values, setValues] = useState("");
-   const [list, setList] = useState(valuesArray);
-   const [number, setNumber] = useState(0);
+   const [valuesEdit, setValuesEdit] = useState({});
+
+   const [list, setList] = useState(initValuesArray);
+   const [number, setNumber] = useState(0); //questions
+
+   const [indexEdit, setIndexEdit] = useState("");
+
    
+
    const handleChange = (e) => {
       e.preventDefault();
       setValues(e.target.value);
+      setIndexEdit("");
    }
 
-   const handleClick = () => {
-      valuesArray.push(values);
-      console.log(valuesArray);
+   const handleChangeEdit = (e) => {
+      e.preventDefault();
+      const index = e.target.name;
+      setValuesEdit({index: index, value:e.target.value});
+   }
 
+   const handleClickAdd = () => {
+      if (values !== "") {
+         valuesArray.push(values);
+      } 
+      
+      console.log(valuesArray);
       setList(valuesArray);
       console.log(list);
 
-      setNumber(valuesArray.length);
+      setNumber(valuesArray.length);  //question
+
+      setValues("");
+      setIndexEdit("");
    }
 
 
    const handleRemoveClick = (event) => {
-      console.log(event.target.item_number); //problem
-      valuesArray.splice(event.target.item_number, 1);
-      setList(valuesArray);
-      console.log(list);
+      const index = event.target.attributes.index_item.value;
+      valuesArray.splice(+index, 1);
 
-      setNumber(valuesArray.length);
+      setList(valuesArray);
+      setNumber(valuesArray.length); //question
    }
 
-   useEffect(() => {
-      console.log(number);
-   }, [number]);
+   
+
+   const handleEditClick = (e) => {
+      const index = e.target.attributes.index_item.value;
+      setIndexEdit(+index);
+   }
+
+   const handleDoneClick = (e) => {
+      valuesArray.splice(+valuesEdit.index, 1, valuesEdit.value);
+
+      setList(valuesArray);
+      setNumber(valuesArray.length); //question
+
+      setIndexEdit("");
+   };
+
+
+   // useEffect(() => {
+   //    console.log(list.length);
+   //    console.log(list);
+
+   // }, [list]);
+
+   console.log(list);
+
+   // const objToJson = JSON.stringify(list);
+   // localStorage.setItem(props.authedName.name,objToJson);
 
    const todo_list = list.map(function(element,index) {
+      
+
       return (<div key={element} className="todoPage__list_item">
-         <p>{index+1}. {element}</p>
+         <p>{index + 1}. {index !== indexEdit ?
+          element :
+           <InputHooks value={element}
+            className="todoPage_inputEdit"
+            onChange={handleChangeEdit}
+            name={index} />}</p>
          <div>
-            <Button text ="EDIT"/>
-            <Button text ="REMOVE" onClick={handleRemoveClick} item_number={index}/>
+            <Button text={index !== indexEdit ? "EDIT" : "DONE"}
+               onClick={index !== indexEdit ? handleEditClick : handleDoneClick}
+               index_item={index} />
+            <Button text="REMOVE"
+               onClick={handleRemoveClick}
+               index_item={index} />
          </div>
-         </div>
+      </div>
       )
    });
 
-   
-
-   
-   // useEffect(() => {
-   // }, [valuesArray]);
-
-
    return (
       <div className="todoPage">
-         <InputHooks onChange={handleChange} value={values} className="todoPage__input"/>
-         <Button text="ADD" onClick={handleClick}/>
+         <InputHooks onChange={handleChange} value={values} className="todoPage__input" placeholder="Type your task..."/>
+         <Button text="ADD" onClick={handleClickAdd}/>
          {todo_list}
       </div>
    )
